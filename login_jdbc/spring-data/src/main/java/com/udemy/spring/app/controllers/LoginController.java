710377c28a87b9e1683f5430bd95f2225b8e3c01
Luis <lvicencio@gmail.com>
2019-10-27 19:00:31 -0300

    login usando jdbc

---
+++ login_jdbc/spring-data/src/main/java/com/udemy/spring/app/controllers/LoginController.java
@@ -0,0 +1,35 @@
+package com.udemy.spring.app.controllers;
+
+import java.security.Principal;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+@Controller
+public class LoginController {
+
+	@GetMapping("/login")
+	public String login(@RequestParam(value = "error", required = false ) String error,	
+			@RequestParam(value = "logout", required = false ) String logout,		
+			Model model, Principal principal, RedirectAttributes flash) {
+		
+		if (principal != null) {
+			flash.addFlashAttribute("info", "Ya se encuentra logeado");
+			return "redirect:/";
+		}
+		
+		if (error != null) {
+			model.addAttribute("error", "Error, UserName o Contraseña incorrecta, Reintente nuevamente");
+		}
+		
+		if (logout != null) {
+			model.addAttribute("success", "Ha Cerrado Sessión con Exito");
+		}
+		
+		return "login";
+	}
+	
+}
